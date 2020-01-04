2041be34dbd55107c2a848caed958eca98be665f
Maduranga <mg.madugamage@gmail.com>
2020-01-04 22:33:52 +0530

    remove typo in rest mapping

--- spring-boot-redis-cache/src/main/java/com/redis/springbootrediscache/HelloWorldController.java
+++ spring-boot-redis-cache/src/main/java/com/redis/springbootrediscache/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping(path = "/hellow")
+    @GetMapping(path = "/hello")
     public String helloWorld() {
         return "Hello";
     }
